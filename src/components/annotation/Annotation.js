82b32a49bcbfbf9ab0a890de759e78f3f920053d
Marek <sinthoras@hotmail.sk>
2023-02-17 18:23:35 +0100

    Fixed lint warnings

--- src/components/annotation/Annotation.js
+++ src/components/annotation/Annotation.js
@@ -1,5 +1,5 @@
 import styles from './Annotation.module.scss';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
 
 export default function Annotation() {
--- src/components/index/Index.js
+++ src/components/index/Index.js
@@ -1,5 +1,5 @@
 import styles from './Index.module.scss';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export default function Index() {
   return (
